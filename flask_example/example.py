43b8a4a5b1f8c5f446c9f2b75acf3920edcbb293
Andrey Bogatyrev <t2way@yandex.ru>
2023-05-11 17:26:21 +0300

    refact example.py: add load_users()

--- flask_example/example.py
+++ flask_example/example.py
@@ -14,7 +14,6 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 #          {'first_name': 'kamila', 'tel': '8902553333'}
 #     ]
 
-# ID = 0
 
 @app.route('/')
 def hello_world():
@@ -50,21 +49,18 @@ def get_user(id):
     return render_template('users/show.html', name=id)
 
 
-# def load_users():
-#     #filename = 'users.json'
-#     filename = '/home/abo/hexlet-flask-example/flask_example/flask_example/users.json'
-#     with open(filename) as f:
-#         users = json.loads(f.read())
-#     return users
+def load_users():
+    #filename = 'users.json'
+    filename = '/home/abo/hexlet-flask-example/flask_example/flask_example/templates/users/users.json'
+    with open(filename) as f:
+        users = json.loads(f.read())
+    return users
 
 
 @app.route('/users/')
 def search_user():
     term = request.args.get('term', '', type=str)
-    #users = load_users()
-    filename = '/home/abo/hexlet-flask-example/flask_example/flask_example/templates/users/users.json'
-    with open(filename) as f:
-        users = json.loads(f.read())
+    users = load_users()
     filtered_users = []
     for user in users:
         pattern = user['first_name']
@@ -79,11 +75,7 @@ def search_user():
 
 @app.post('/users/')
 def users_post():
-    # global ID
-    # ID += 1
-    filename = '/home/abo/hexlet-flask-example/flask_example/flask_example/templates/users/users.json'
-    with open(filename) as f:
-        users = json.loads(f.read())
+    users = load_users()
     user = request.form.to_dict()
     user['id'] = str((int(users[-1]['id']) + 1))
     users.append(user)
